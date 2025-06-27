9e237418b6556cd7c2f5476a87c75bc067f676d9
RoushanSri <roshansrivastav59796@gmail.com>
2025-06-27 16:03:31 +0530

    Add filtering functionality to PastEvents component;

--- frontend/src/pages/PastEvents.jsx
+++ frontend/src/pages/PastEvents.jsx
@@ -4,12 +4,24 @@ import { getPastEvents } from '../redux/slice/splitSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { useState } from 'react'
 import SplitCard from '../components/SplitCard.jsx'
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import { FaFilter } from 'react-icons/fa'
 
 const PastEvents = () => {
   const dispatch = useDispatch()
   const { splits } = useSelector((state) => state.split)
 
   const [splitData, setSplitData] = useState([])
+  const [status, setStatus] = useState('')
+  const [filteredData, setFilteredData] = useState([])
+
+  const handleChange = (event) => {
+    setStatus(event.target.value)
+    setFilteredData(splitData.filter((split) => {
+      if (event.target.value === '') return true
+      return split.status === event.target.value
+    }))
+  }
 
   useEffect(() => {
     if (splits.length !== 0) {
@@ -34,6 +46,7 @@ const PastEvents = () => {
         moreItems: split.items.length > 2 ? split.items.length - 2 : 0,
       }))
       setSplitData(data)
+      setFilteredData(data)
     }
   }, [splits])
 
@@ -45,11 +58,30 @@ const PastEvents = () => {
   return (
     <div>
       <div className="min-h-[50vh] w-full p-2 mt-4 rounded-2xl">
-        <h1 className="font-semibold text-2xl text-gray-900 bg-white p-2 rounded-xl">
+        <div className="flex items-center justify-between bg-white p-2 rounded-xl">
+          <h1 className="font-semibold text-2xl text-gray-900 px-2">
           Past Events
         </h1>
+        <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
+          <InputLabel id="demo-select-small-label"><div className='flex gap-2 items-center'><FaFilter/><h1>Filter</h1></div></InputLabel>
+          <Select
+            labelId="demo-select-small-label"
+            id="demo-select-small"
+            value={status}
+            label="Status"
+            onChange={handleChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={"pending"}><span className='bg-yellow-100 text-yellow-800 px-3 py-1 rounded-xl text-sm'>Pending</span></MenuItem>
+            <MenuItem value={"completed"}><span className='bg-green-100 text-green-800 px-3 py-1 rounded-xl text-sm'>Completed</span></MenuItem>
+          </Select>
+        </FormControl>
+        </div>
+        
         <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 my-6">
-          {splitData.map((split, idx) => (
+          {filteredData.map((split, idx) => (
             <SplitCard key={idx} split={split}/>
         ))}
         </div>
